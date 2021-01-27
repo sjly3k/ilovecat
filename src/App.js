800f22dc5ad26e01f321483fb5d3316cb7f5bca4
sjly3k <sjly3k@naver.com>
2021-01-27 17:46:19 +0900

    Commit : UI Design + API

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import SearchBar from './components/SearchBar.js';
+import SearchResult from './components/SearchResult.js';
+import SearchInfo from './components/SearchInfo.js';
+import { api } from './api/theCatAPI.js';
+
+export default class App {
+	constructor() {
+		const top = document.createElement('div');
+		top.className = 'top';
+
+		const bottom = document.createElement('div');
+		bottom.className = 'bottom';
+
+		const modal = document.createElement('div');
+		modal.className = 'modal';
+		modal.classList.add('hidden');
+
+		const searchBar = new SearchBar(top,
+			keyword => {
+				api.fetchImage(keyword).then(data => {
+					searchResult.updateData(data);
+				});
+			},
+			() => {
+				api.fetchImageAll().then(data => {
+					searchResult.updateData(data);
+				});
+			}
+		);
+
+		const searchResult = new SearchResult(bottom,
+			target => {
+				searchInfo.updateData(target.data);
+				modal.classList.toggle('hidden');
+			}
+		);
+
+		const searchInfo = new SearchInfo(modal);
+
+		document.body.appendChild(top);
+		document.body.appendChild(bottom);
+		document.body.appendChild(modal);
+	}
+}
---
+++ src/api/theCatAPI.js
@@ -0,0 +1,34 @@
+const API_ENDPOINT = 'https://api.thecatapi.com/v1';
+
+const request = async (url) => {
+	try {
+		const result = await fetch(url);
+		return result.json();
+	} catch (e) {
+		console.warn(e)
+	}
+}
+
+export const api = {
+	fetchImage: async keyword => {
+		const breeds = (await api.searchBreedByName(keyword)).map(breed => { return breed.id });
+		const requests = breeds.map(breed => {
+			return request(`${API_ENDPOINT}/images/search?limit=50&breed_ids=${breed}`);
+		})
+
+		return Promise.all(requests).then(responses => {
+			let result = [];
+			responses.forEach(response => {
+				result = result.concat(response)
+			})
+
+			return result;
+		})
+	},
+	fetchImageAll: () => {
+		return request(`${API_ENDPOINT}/images/search?limit=50`)
+	},
+	searchBreedByName: keyword => {
+		return request(`${API_ENDPOINT}/breeds/search?q=${keyword}`)
+	}
+}
